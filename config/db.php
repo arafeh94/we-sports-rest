7d29f852490ffb67b8d1a835e80d4b3520605dca
Arafeh <arafeh198@gmail.com>
2018-02-12 15:44:48 +0200

    init

--- config/db.php
+++ config/db.php
@@ -2,11 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mappers',
+    'dsn' => 'mysql:host=localhost;dbname=none',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
--- modules/api/v1/Module.php
+++ modules/api/v1/Module.php
@@ -8,11 +8,38 @@
 
 namespace app\modules\api\v1;
 
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\Cors;
+
 class Module extends \yii\base\Module
 {
     public function init()
     {
         parent::init();
         \Yii::configure($this, require __DIR__ . '/config/web.php');
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+            ],
+        ];
+
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::className(),
+            'except' => ['options', 'login', 'index/index'],
+        ];
+
+        return $behaviors;
     }
 }
